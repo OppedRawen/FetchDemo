0cc6d933630ecb08a0b444f90b59b5aab8513ceb
OppedRawen <yud2372@gmail.com>
2023-11-17 02:49:34 -0800

    fourth commit

--- assets/result.js
+++ assets/result.js
@@ -3,15 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     function getQuery(param){
-        var queryString = window.location.search.substring(1);
-        var params = queryString.split('&');
-        for(var i=0; i<params.length; i++){
-            var pair = params[i].split('=');
-            if(pair[0] === param){
-                return decodeURIComponent(pair[1]);
-            }
-        }
-        return null;
+       const params = new URLSearchParams(window.location.search);
+         return params.get(param);
     }
 
     var city = getQuery('city');
@@ -23,17 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('city-name').textContent = 'No city found';
     }
 
-    // function to fetch restaurants data
-
 function fetchRestaurants(cityName) {
-    // Yelp Fusion API endpoint for business search
     var url = 'https://api.yelp.com/v3/businesses/search';
 
     var proxyUrl = 'https://afternoon-badlands-11870.herokuapp.com/'; // Use a proxy to avoid CORS errors
-    // Use a placeholder for the API key
     var apiKey ='NKox_GY98ntD7Hu5CCjMS8VQevvrNWZf69qUTAZhXNKLAgrVQPM9M7se2ktuWnZMrU0OPOUdUc1X2Gjp6kzEVPW632s2UUHTr0sRi8h3aVxZ9F752QYGW5blZxpXZXYx';
 
-    // Making a fetch request directly to Yelp's Business Search endpoint
     fetch(proxyUrl+url+`?location=${encodeURIComponent(cityName)}`, {
         method: 'GET',
         headers: {
@@ -45,23 +33,25 @@ function fetchRestaurants(cityName) {
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
+        // parses response to json if successful
+        console.log(response, 'response');
         return response.json();
     })
     .then(data => {
         displayRestaurants(data.businesses);
-        console.log(data.businesses);
+       console.log(data, 'data')
     })
     .catch(error => {
         console.error('Fetch error:', error);
-        // Optionally handle the error on the UI, e.g., show an error message
+
     });
 }
 function displayRestaurants(data) {
     var grid = document.getElementById('results-grid');
-    // Clear any existing content
+
     grid.innerHTML = '';
 
-    // Loop through restaurant data and create elements for each
+  
     var restaurantElements = data.map(function(restaurant) {
         var restaurantElement = document.createElement('div');
         restaurantElement.className = 'restaurant';
